4ea3d1a9a298851a08acff04f15da74e86802694
@mosbat <HackathonMos@Moss.(none)>
2022-10-01 20:55:02 +0200

    Added response model for NEO

---
+++ src/main/java/org/spaceappchallenge/asteroidseeker/model/AsteroidResponseDTO.java
@@ -0,0 +1,53 @@
+package org.spaceappchallenge.asteroidseeker.model;
+
+import java.time.LocalDateTime;
+
+public class AsteroidResponseDTO {
+
+    private String fullNameOfNEO;
+    private String orbitId;
+    private String designationCode;
+    private double diameter;
+    private double distance;
+    private LocalDateTime timeOfCloseApproach;
+    private double velocity;
+    private String sigma;
+    private String orbitClass;
+
+
+    public String getFullNameOfNEO() {
+        return fullNameOfNEO;
+    }
+
+    public String getOrbitId() {
+        return orbitId;
+    }
+
+    public String getDesignationCode() {
+        return designationCode;
+    }
+
+    public double getDiameter() {
+        return diameter;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public LocalDateTime getTimeOfCloseApproach() {
+        return timeOfCloseApproach;
+    }
+
+    public double getVelocity() {
+        return velocity;
+    }
+
+    public String getOrbitClass() {
+        return orbitClass;
+    }
+
+    public String getSigma() {
+        return sigma;
+    }
+}
