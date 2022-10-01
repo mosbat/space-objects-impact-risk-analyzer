397fbbbf526082b916d91903e8d609ee19baa60e
@mosbat <HackathonMos@Moss.(none)>
2022-10-01 18:02:24 +0200

    Base files

---
+++ src/main/java/org/spaceappchallenge/asteroidseeker/AsteroidseekerApplication.java
@@ -0,0 +1,13 @@
+package org.spaceappchallenge.asteroidseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteroidseekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsteroidseekerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/spaceappchallenge/asteroidseeker/AsteroidseekerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spaceappchallenge.asteroidseeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsteroidseekerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
